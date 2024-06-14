e975e5a37877d71d6733879969a94d467f5b162e
Bobsojack <bobsojack21910@gmail.com>
2024-06-14 16:12:32 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
+Route::get('auth/login', function () {
     return view('auth/login');
-});
+})->name('auth/login');
 
 Route::get('about', function () {
     $name="peerawat";
@@ -14,8 +14,16 @@ Route::get('about', function () {
 });
 
 
-
-
+Route::get('dashboard', function(){
+    return view('dashboard');
+});
+Route::get('formEquiment/importEquiment', function(){
+    return view('formEquiment/importEquiment');
+});
+//exportEquiment
+Route::get('formEquiment/exportEquiment', function(){
+    return view('formEquiment/exportEquiment');
+});
 
 Route::fallback(function(){
     return '<h1>ไม่พบหน้าเว็บ</h1>';
@@ -23,7 +31,12 @@ Route::fallback(function(){
 });
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('admin/home',[Homecontroller::class, 'adminHome'])->name('admin.home')->Middleware('is_admin');
- 
+// routes/web.php
+
+// Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+// Route::post('register', [RegisterController::class, 'register']);
+// Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+// Route::post('login', [LoginController::class, 'login']);
